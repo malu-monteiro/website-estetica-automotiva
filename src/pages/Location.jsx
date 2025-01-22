582e6a28c496d211b76c4e2f8647c063fd02248a
Maria Luiza <malumonteiro.dev@gmail.com>
2025-01-22 19:07:47 -0300

    add: map.css + refatoramento location

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
+import "../styles/map.css";
 
 export default function Localization() {
   const mapContainer = useRef(null);
@@ -12,6 +13,8 @@ export default function Localization() {
   maptilersdk.config.apiKey = apiKey;
 
   const createPopup = (lng, lat, title, address, cep) => {
+    const fullAddress = `${address} - CEP: ${cep}`;
+
     return new maptilersdk.Popup({
       offset: 25,
       closeButton: false,
@@ -20,9 +23,32 @@ export default function Localization() {
     })
       .setLngLat([lng, lat])
       .setHTML(
-        `<h3 style="color: black;">${title}</h3>
-        <p style="color: black;">${address}</p>
-        <p style="color: black;">CEP: ${cep}</p>`
+        `<div class="map-popup">
+        <div class="map-popup__container">
+          <h3 class="map-popup__title">${title}</h3>
+          <div class="map-popup__button-wrapper">
+            <button 
+              onclick="
+                navigator.clipboard.writeText('${fullAddress}');
+                const tooltip = this.querySelector('.map-popup__tooltip');
+                tooltip.style.opacity = '1';
+                setTimeout(() => tooltip.style.opacity = '0', 2000);
+              "
+              class="map-popup__button"
+              title="Copiar endereço"
+            >
+              <div class="map-popup__tooltip">Copiado!</div>
+              <svg width="16" height="16" viewBox="0 0 24 24" style="stroke: currentColor;">
+                <rect width="8" height="4" x="8" y="2" rx="1" ry="1" fill="none" stroke-width="2"/>
+                <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" fill="none" stroke-width="2"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <p class="map-popup__text">${address}</p>
+        <p class="map-popup__text">CEP: ${cep}</p>
+      </div>
+      `
       );
   };
 
