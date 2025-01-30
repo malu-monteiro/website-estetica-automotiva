1d9bfe20b2f02601962ded01ce97c60d3d1d02ce
Maria <malumonteiro.dev@gmail.com>
2025-01-30 18:08:47 -0300

    add: Home refactor

---
+++ src/components/FeatureList.jsx
@@ -0,0 +1,38 @@
+import CountUp from "react-countup";
+
+import PropTypes from "prop-types";
+
+export const FeatureList = ({ features }) => (
+  <div className="grid md:grid-cols-2 gap-8 mt-52">
+    {features.map((stat) => (
+      <div key={stat.id} className="flex items-start space-x-4">
+        <stat.icon className="shrink-0 h-16 w-16 md:h-20 md:w-20 text-white" />
+        <div>
+          <h4 className="text-3xl md:text-4xl font-bold text-white">
+            <CountUp
+              start={0}
+              end={parseInt(stat.title.replace(/\D/g, ""))}
+              duration={2.5}
+              separator=""
+            />
+            {stat.title.replace(/[0-9]/g, "")}
+          </h4>
+          <p className="text-base md:text-lg bg-gradient-to-r from-white to-gray-400 text-transparent bg-clip-text">
+            {stat.content}
+          </p>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+FeatureList.propTypes = {
+  features: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      icon: PropTypes.elementType.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
-import { homeData } from "../constants/homeConstants";
+import { homeData } from "../constants/index.jsx";
 import { features } from "../constants";
 
-import CountUp from "react-countup";
-
+import { FeatureList } from "../components/FeatureList";
 import Scheduling from "../components/Scheduling";
 import { Button } from "../components/Button";
 
@@ -37,27 +36,7 @@ export default function Home() {
           </Button>
         </div>
 
-        <div className="grid md:grid-cols-2 gap-8 mt-52">
-          {features.map((stat) => (
-            <div key={stat.id} className="flex items-start space-x-4">
-              <stat.icon className="shrink-0 h-16 w-16 md:h-20 md:w-20 text-white" />
-              <div>
-                <h4 className="text-3xl md:text-4xl font-bold text-white">
-                  <CountUp
-                    start={0}
-                    end={parseInt(stat.title.replace(/\D/g, ""))}
-                    duration={2.5}
-                    separator=""
-                  />
-                  {stat.title.replace(/[0-9]/g, "")}
-                </h4>
-                <p className="text-base md:text-lg bg-gradient-to-r from-white to-gray-400 text-transparent bg-clip-text">
-                  {stat.content}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <FeatureList features={features} />
       </div>
     </section>
   );
