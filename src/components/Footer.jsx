8d90fd67222f0f50a597471e3e89f993b80fd886
Maria <malumonteiro.dev@gmail.com>
2025-02-03 22:56:15 -0300

    refactor(Footer): improve structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,12 @@ import { SocialMediaSection } from "../components/SocialMediaSection";
 
 export default function Footer() {
   return (
-    <footer id="contact" className="mt-20 border-t py-10 border-neutral-700">
-      <div className="container mx-auto flex flex-col items-center">
-        <div className="flex flex-col md:flex-row justify-between items-start w-full gap-4">
+    <footer
+      id="contact"
+      className="mt-16 md:mt-20 border-t py-8 md:py-10 border-neutral-700"
+    >
+      <div className="max-w-7xl mx-auto px-6 md:px-8">
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-start w-full gap-8 md:gap-4">
           <AboutSection aboutUs={aboutUs} />
           <ContactSection contact={contact} />
           <SocialMediaSection socialMedia={socialMedia} />
