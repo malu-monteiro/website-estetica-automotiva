2bb0f90ed00e76234b7f541ceabc95cc51e0182b
Maria <malumonteiro.dev@gmail.com>
2025-02-03 22:59:58 -0300

    refactor(SocialMediaSection): add icons

--- src/components/SocialMediaSection.jsx
+++ src/components/SocialMediaSection.jsx
@@ -4,18 +4,22 @@ export const SocialMediaSection = ({ socialMedia }) => (
   <div className="flex flex-col items-center md:items-start">
     <h3 className="text-md font-semibold mb-4">Redes Sociais</h3>
     <ul className="flex justify-center md:justify-start space-x-4">
-      {socialMedia.map((link) => (
-        <li key={link.id}>
-          <a
-            href={link.href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-neutral-300 text-2xl hover:text-white"
-          >
-            {link.icon}
-          </a>
-        </li>
-      ))}
+      {socialMedia.map((link) => {
+        const Icon = link.icon;
+        return (
+          <li key={link.id}>
+            <a
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-neutral-300 text-2xl hover:text-white"
+              aria-label={link.text}
+            >
+              <Icon />
+            </a>
+          </li>
+        );
+      })}
     </ul>
   </div>
 );
@@ -25,7 +29,8 @@ SocialMediaSection.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       href: PropTypes.string.isRequired,
-      icon: PropTypes.element.isRequired,
+      icon: PropTypes.elementType.isRequired,
+      text: PropTypes.string,
     })
   ).isRequired,
 };
