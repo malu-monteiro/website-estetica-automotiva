4529115044bbd87a204037d379da014cdd1ddb20
Maria Luiza <malumonteiro.dev@gmail.com>
2025-05-25 03:45:36 -0300

    fix: align footer and sections containers with navbar for consistent layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,14 +33,14 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-between items-center">
+      <div className="container px-2 mx-auto relative text-sm">
+        <div className="flex items-center flex-shrink-0">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-12 w-25 mr-2 md:ml-16" src={logo} alt="logo" />
+            <img className="h-12 w-25 mr-2 md:ml-20" src={logo} alt="logo" />
             <span className="text-lg tracking-tight">MSS_STUDIO CAR</span>
           </div>
 
-          <ul className="hidden lg:flex ml-14 space-x-12">
+          <ul className="hidden lg:flex ml-40 space-x-12">
             {navLinks.map((item, index) => (
               <li key={index}>
                 <a
@@ -54,7 +54,7 @@ export default function Navbar() {
             ))}
           </ul>
 
-          <div className="lg:hidden md:flex flex-col justify-end">
+          <div className="lg:hidden md:flex flex-col justify-end ml-4 md:ml-0">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? <X /> : <Menu />}
             </button>
