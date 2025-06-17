f6a9477bb8942bd4952199891f9364fc9b4f5a86
Maria Luiza <malumonteiro.dev@gmail.com>
2025-06-17 19:10:48 -0300

    add: page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div
+      className="fixed inset-0 flex flex-col items-center justify-start bg-black text-white p-4 overflow-hidden"
+      style={{ top: 0, left: 0, right: 0, bottom: 0 }}
+    >
+      <div className="pt-20 text-center">
+        <Image
+          src="/404-error.svg"
+          alt="Erro 404 - Página não encontrada"
+          width={400}
+          height={300}
+          className="mx-auto mb-8 rounded-lg shadow-lg"
+        />
+        <h2 className="text-3xl font-bold mb-4">Página não encontrada.</h2>
+        <p className="text-lg mb-8">
+          Parece que a página que você procura não existe ou foi removida.
+        </p>
+        <div className="flex flex-col items-center gap-2">
+          <Link href="/" className="text-blue-500 hover:underline text-base">
+            Voltar para a Página Inicial
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
