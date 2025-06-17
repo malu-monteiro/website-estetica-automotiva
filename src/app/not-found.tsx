6b82af02b87167a04140528e5c086c261032792c
Maria Luiza <malumonteiro.dev@gmail.com>
2025-06-17 20:18:06 -0300

    .

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,10 +15,13 @@ export default function NotFound() {
           height={300}
           className="mx-auto mb-8 rounded-lg shadow-lg"
         />
+
         <h2 className="text-3xl font-bold mb-4">Página não encontrada.</h2>
+
         <p className="text-lg mb-8">
           Parece que a página que você procura não existe ou foi removida.
         </p>
+
         <div className="flex flex-col items-center gap-2">
           <Link href="/" className="text-blue-500 hover:underline text-base">
             Voltar para a Página Inicial
